ccd42f2e807b7870071a6090350e3b1d632bdd9a
parklaus1078 <parklaus1078@gmail.com>
2022-08-18 14:47:41 +0900

    utils 폴더에 dataSource.js 파일 추가

---
+++ utils/dataSource.js
@@ -0,0 +1,24 @@
+const {DataSource} = require('typeorm');
+
+const myDataSource = new DataSource({
+    type: process.env.TYPEORM_CONNECTION,
+    host: process.env.TYPEORM_HOST,
+    port: process.env.TYPEORM_PORT,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE
+});
+
+myDataSource.initialize()
+            .then(() => {
+                console.log("Data source has been initialized!");
+            })
+            .catch((err) => {
+                console.log("Error occured during data intialization", err)
+
+                myDataSource.destroy();
+            });
+
+module.exports = {
+    myDataSource
+}
