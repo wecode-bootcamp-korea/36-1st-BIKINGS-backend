08e97915a098dcfc4be693a8da4c8c14346800aa
parklaus1078 <parklaus1078@gmail.com>
2022-08-18 10:13:10 +0900

    수정 2 - products table에 stock과 price 항목 추가

--- db/migrations/20220817104103_products.sql
+++ db/migrations/20220817104103_products.sql
@@ -3,7 +3,9 @@ CREATE TABLE products (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     cover_image_url VARCHAR(2038) NULL,
-    detail_image_url VARCHAR(2038) NULL
+    detail_image_url VARCHAR(2038) NULL,
+    stock INT NOT NULL DEFAULT 0,
+    price DECIMAL(8,2) NOT NULL
 );
 
 -- migrate:down
