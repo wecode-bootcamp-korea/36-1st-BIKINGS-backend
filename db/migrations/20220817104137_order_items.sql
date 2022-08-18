e8ddf354df56f583439d803002bf2f5d5bbb5f77
parklaus1078 <parklaus1078@gmail.com>
2022-08-18 16:26:48 +0900

    수정5 - orders와 order_items 관계 수정

--- db/migrations/20220817104137_order_items.sql
+++ db/migrations/20220817104137_order_items.sql
@@ -4,8 +4,10 @@ CREATE TABLE order_items (
     amount INT NOT NULL,
     product_id INT NOT NULL,
     status_order_item_id INT NOT NULL,
+    order_id INT NOT NULL,
     CONSTRAINT fk_order_items_product_id FOREIGN KEY (product_id) REFERENCES products (id),
-    CONSTRAINT fk_order_items_status_order_item_id FOREIGN KEY (status_order_item_id) REFERENCES status_order_items (id)
+    CONSTRAINT fk_order_items_status_order_item_id FOREIGN KEY (status_order_item_id) REFERENCES status_order_items (id),
+    CONSTRAINT fk_order_items_order_id FOREIGN KEY (order_id) REFERENCES orders (id)
 );
 
 -- migrate:down
