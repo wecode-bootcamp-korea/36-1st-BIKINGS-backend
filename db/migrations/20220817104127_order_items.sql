4257f4d47078b38efd574d4f328d38b9009b378a
parklaus1078 <parklaus1078@gmail.com>
2022-08-18 16:05:13 +0900

    orders와 order_items tables의 관계 수정

--- db/migrations/20220817104127_order_items.sql
+++ db/migrations/20220817104127_order_items.sql
@@ -2,10 +2,8 @@
 CREATE TABLE order_items (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     amount INT NOT NULL,
-    user_id INT NOT NULL,
     product_id INT NOT NULL,
     status_order_item_id INT NOT NULL,
-    CONSTRAINT fk_order_items_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
     CONSTRAINT fk_order_items_product_id FOREIGN KEY (product_id) REFERENCES products (id),
     CONSTRAINT fk_order_items_status_order_item_id FOREIGN KEY (status_order_item_id) REFERENCES status_order_items (id)
 );
--- db/migrations/20220817104135_orders.sql
+++ db/migrations/20220817104135_orders.sql
@@ -5,7 +5,7 @@ CREATE TABLE orders (
   order_item_id INT NOT NULL,
   status_order_id INT NOT NULL,
   user_address_id INT NOT NULL,
-  CONSTRAINT fk_orders_user_id FOREIGN KEY (user_id) REFERENCES users (id),
+  CONSTRAINT fk_orders_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
   CONSTRAINT fk_orders_order_item_id FOREIGN KEY (order_item_id) REFERENCES order_items (id),
   CONSTRAINT fk_orders_status_order_id FOREIGN KEY (status_order_id) REFERENCES status_orders (id),
   CONSTRAINT fk_orders_user_address_id FOREIGN KEY (user_address_id) REFERENCES user_addresses (id)
