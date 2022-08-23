216c42799c2005eaab212554c1468161c99afc11
parklaus1078 <parklaus1078@gmail.com>
2022-08-23 18:02:04 +0900

    수정 11 - query문 만으로 DB에서 데이터 불러오기 완료

--- models/productDao.js
+++ models/productDao.js
@@ -1,7 +1,6 @@
 const {myDataSource} = require('../utils/dataSource');
 
 const getProductIds = async () => {
-    try {
         const productIds = await myDataSource.query(
                             `SELECT
                                 JSON_ARRAYAGG(
@@ -9,20 +8,11 @@ const getProductIds = async () => {
                                 ) AS ids
                             FROM products
                             `);
-        
-        const len = productIds[0].ids.length;
-        const ids = productIds[0].ids.slice(1,len-1).split(', ');
 
-        return ids;
-    } catch (err) {
-        const error = new Error("SOMETHING IS WRONG");
-        error.statusCode = 500;
-        throw error;
-    }
+        return productIds[0].ids;
 };
 
 const getProductCovers = async (limit, offset) => {
-    try {
         const productInfo = await myDataSource.query(
             `SELECT 
                 p.id,
@@ -31,29 +21,19 @@ const getProductCovers = async (limit, offset) => {
                 p.price,
                 JSON_ARRAYAGG(
                     t.name
-                ) AS tags
-                FROM products p
-                JOIN tag_bunches tb
-                ON tb.product_id = p.id
-                INNER JOIN tags t
-                ON t.id = tb.tag_id
-                GROUP BY p.id
-                ORDER BY p.id
-                LIMIT ${limit}
-                OFFSET ${offset}`
+                    ) AS tags
+            FROM products p
+            JOIN tag_bunches tb
+            ON tb.product_id = p.id
+            INNER JOIN tags t
+            ON t.id = tb.tag_id
+            GROUP BY p.id
+            ORDER BY p.id
+            LIMIT ${limit}
+            OFFSET ${offset}`
         );
 
-        productInfo.map((obj) => {
-            const len = obj.tags.length;
-            obj.tags = obj.tags.slice(2,len-2).split('", "');
-        });
-
         return productInfo;
-    } catch (err) {
-        const error = new Error("INVALID");
-        error.statusCode = 500;
-        throw error;
-    }
 };
 
 module.exports = {
