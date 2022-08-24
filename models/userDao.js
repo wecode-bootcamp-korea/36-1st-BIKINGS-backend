005927cea5235604af60c3132ad890f51828fb5b
lee sock won <lsw4uto@gmail.com>
2022-08-24 18:02:11 +0900

    [수정]]feature/sockwon/users/signup

---
+++ models/userDao.js
@@ -0,0 +1,57 @@
+const {myDataSource} = require("../utils/dataSource");
+
+const createUser = async(name,username, hashed, birth, contact, point)=>{
+        return await myDataSource.query(
+            `
+            INSERT INTO users(
+                name,
+                username,
+                password,
+                birth,
+                contact,
+                point
+            )VALUES (
+                ?,?,?,?,?,?
+            )
+            `,[name,username, hashed, birth, contact, point]
+        )
+}
+
+const findUserId = async(username)=>{
+        return await myDataSource.query(
+            `
+            SELECT users.id
+            FROM users
+            WHERE users.username = "${username}"
+            `
+        )
+}
+
+const createUserAddress = async (id, address)=>{
+        return await myDataSource.query(
+            `
+            INSERT INTO user_addresses (
+            user_id,
+            address
+            ) VALUES (
+                ?,?
+            )
+            `,[id, address]
+        )
+}
+
+const isNew=async(username)=>{
+        return await myDataSource.query(
+        `
+        SELECT EXISTS(
+            SELECT *
+            FROM users 
+            WHERE users.username = "${username}"
+            )
+        `
+        )
+}
+
+module.exports={
+    createUser,isNew, createUserAddress, findUserId
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const userRouter = require('./userRouter');
 const productRouter = require('./productRouter');
 
 router.use('', productRouter.router);
+router.use('/users', userRouter.router);
 
 module.exports = router;
