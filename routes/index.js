9f4ede1f38113445d3be847c350bb262bfde99c2
parklaus1078 <parklaus1078@gmail.com>
2022-08-25 18:59:41 +0900

    수정2-index.js 수정

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const productRouter = require('./productRouter');
 const userRouter = require('./userRouter');
 const cartRouter = require("./cartRouter");
+const orderRouter = require("./orderRouter");
 
 router.use('', productRouter.router);
 router.use('/users', userRouter.router);
+router.use('/products', productRouter.router);
 router.use('/carts', cartRouter.router);
+router.use('/orders', orderRouter.router);
 
 module.exports =router
